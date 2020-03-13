aaa8b3312554b4ffc384f7e9fa9e3e2c935320a3
darinmarinov <darinmarinov@users.noreply.github.com>
2020-03-13 12:05:07 +0200

    Setup and Make Get request

---
+++ src/CardsList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Cards = () => {
+  return (
+    <React.Fragment>
+        <div className="cards">
+
+        </div>
+    </React.Fragment>
+  );
+}
+
+export default Cards;

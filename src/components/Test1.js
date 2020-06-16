fa1f32310edfff7f51418993b017a915e24afc8e
darinmarinov <darinmarinov@users.noreply.github.com>
2020-06-16 16:21:52 +0300

    Update Test1.js

--- src/components/Test1.js
+++ src/components/Test1.js
@@ -9,3 +9,16 @@ console.log('test0')
 console.log('test2')
 console.log('test3')
 
+function rr(){
+    console.log('1')
+}
+
+function sss(){
+    console.log('2')
+}
+console.log('test0')
+console.log('test2')
+console.log('test3')
+
+
+

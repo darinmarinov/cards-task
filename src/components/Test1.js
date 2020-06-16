12f1d33364331877d9f39f6d0291263f8e88088a
darinmarinov <darinmarinov@users.noreply.github.com>
2020-06-16 16:24:13 +0300

    Update Test1.js

--- src/components/Test1.js
+++ src/components/Test1.js
@@ -1,24 +1,6 @@
 function rr(){
-    console.log('1')
+    console.log('233')
 }
-
-function sss(){
-    console.log('2')
-}
-console.log('test0')
-console.log('test2')
-console.log('test3')
-
-function rr(){
-    console.log('1')
-}
-
-function sss(){
-    console.log('2')
-}
-console.log('test0')
-console.log('test2')
-console.log('test3')
 
 
 
